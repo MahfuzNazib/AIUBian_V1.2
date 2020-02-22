4ad0084ee3357dd678d17dff83e666fcb9f3a652
MahfuzNazib <nazibmahfuz60@gmail.com>
2020-02-22 22:42:57 +0600

    Admin Profile and Editable View Added on Admin View

--- controllers/adminhome.js
+++ controllers/adminhome.js
@@ -5,4 +5,21 @@ router.get('/', function(req, res){
     res.render('adminhome/index');
 });
 
+router.get('/allPosts', function(req, res){
+    res.render('adminhome/posts');
+});
+
+router.get('/chat', function(req, res){
+    res.render('adminhome/chat');
+});
+
+router.get('/profile', function(req, res){
+    res.render('adminhome/profile');
+});
+
+router.get('/editProfile', function(req, res){
+    res.render('adminhome/editProfile');
+});
+
+
 module.exports = router;
