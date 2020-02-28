abca2b456697e1b27264796b727cb775d8c645a2
MahfuzNazib <nazibmahfuz60@gmail.com>
2020-02-28 21:53:49 +0600

    Add Post Lists & Member Lists on Admin Page

--- controllers/adminhome.js
+++ controllers/adminhome.js
@@ -21,5 +21,41 @@ router.get('/editProfile', function(req, res){
     res.render('adminhome/editProfile');
 });
 
+//Faculty Post Request
+router.get('/facultyPost', function(req, res){
+    res.render('adminhome/facultyPost');
+});
+
+//Alumni Post List Request
+
+router.get('/alumniPost', function(req, res){
+    res.render('adminhome/alumniPOst');
+});
+
+//Student post List Request
+router.get('/studentPost', function(req, res){
+    res.render('adminhome/studentPost');
+});
+
+//All Member List Request
+router.get('/allMemberList', function(req, res){
+    res.render('adminhome/allMemberList');
+});
+
+//Faculty Member List Request
+router.get('/facultyMemberList', function(req, res){
+    res.render('adminhome/facultyMemberList');
+});
+
+//Alumni Member List Request
+router.get('/alumniMemberList', function(req, res){
+    res.render('adminhome/alumniMemberList');
+});
+
+//Student Member List Request
+router.get('/studentMemberList', function(req, res){
+    res.render('adminhome/studentMemberList');
+});
+
 
 module.exports = router;
